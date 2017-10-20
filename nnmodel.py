95abc800bf8be07812ec2fe20b0607af825683dd
Manish Meher Edida <edidamanish@gmail.com>
2017-10-21 03:30:51 +0530

    Add files via upload

--- nnmodel.py
+++ nnmodel.py
@@ -106,29 +106,29 @@ model.save(MODEL_NAME)
 #test_data = create_test_set()
 test_data = np.load('test_data.npy')
 
-#import matplotlib.pyplot as plt
-#
-#for num, data in enumerate(test_data[:12]):
-#    img_num = data[1]
-#    img_data = data[0]
-#    
-#    y = fig.add_subplot(3,4,num+1)
-#    orig = num_data
-#    data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
-#    
-#    model_out = model.predict([data])[0]
-#    
-#    if np.argmax(model_out) == 1:
-#        str_label = 'Dog'
-#    else:
-#        str_label = 'Cat'
-#        
-#    y.imshow(orig,cmap = 'grey')
-#    plt.title(str_label)
-#    y.axes.get_xaxis().set_visible(False)
-#    y.axes.get_yaxis().set_visible(False)
-#    
-#plt.show()
+import matplotlib.pyplot as plt
+fig = plt.figure()
+for num, data in enumerate(test_data[:12]):
+    img_num = data[1]
+    img_data = data[0]
+    
+    y = fig.add_subplot(3,4,num+1)
+    orig = img_data
+    data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
+    
+    model_out = model.predict([data])[0]
+    
+    if np.argmax(model_out) == 1:
+        str_label = 'Dog'
+    else:
+        str_label = 'Cat'
+        
+    y.imshow(orig)
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+    
+plt.show()
 
 
 with open('submission_file.csv', 'w') as f:
